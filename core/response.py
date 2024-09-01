bed61139c5f7d664c00a3b8b36c3e4fedaa94877
rambs <micrabom@gmail.com>
2024-09-01 11:38:12 +0800

    update(BaseResponse): update all my base response to ensure all no params cannot return null

--- core/response.py
+++ core/response.py
@@ -3,12 +3,16 @@ from rest_framework import status
 
 class BaseResponse(Response):
     def __init__(self, message=None, error=None, token=None, user=None, status=status.HTTP_200_OK, user_id=None, **kwargs):
-        data = {
-            "message": message,
-            "error": error,
-            "token": token,
-            "user": user,
-            "user_id": user_id,
-        }
+        data = {}
+        if message is not None:
+            data["message"] = message
+        if error is not None:
+            data["error"] = error
+        if token is not None:
+            data["token"] = token
+        if user is not None:
+            data["user"] = user
+        if user_id is not None:
+            data["user_id"] = user_id
         data.update(kwargs)
         super().__init__(data=data, status=status)
