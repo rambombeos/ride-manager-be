44d4690fb6911e1bd1c930ba8f8547ae8204b3d6
rambs <micrabom@gmail.com>
2024-09-01 19:47:07 +0800

    update(BASERESPONSE): adding success params and change position for readability purposes

--- core/response.py
+++ core/response.py
@@ -2,14 +2,16 @@ from rest_framework.response import Response
 from rest_framework import status
 
 class BaseResponse(Response):
-    def __init__(self, message=None, error=None, token=None, user=None, status=status.HTTP_200_OK, user_id=None, **kwargs):
+    def __init__(self, message=None, success=False, token=None, error=None, user=None, status=status.HTTP_200_OK, user_id=None, **kwargs):
         data = {}
+        if success is not None:
+            data["success"] = success
         if message is not None:
             data["message"] = message
-        if error is not None:
-            data["error"] = error
         if token is not None:
             data["token"] = token
+        if error is not None:
+            data["error"] = error
         if user is not None:
             data["user"] = user
         if user_id is not None:
